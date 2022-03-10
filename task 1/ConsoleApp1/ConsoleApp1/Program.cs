e537a01ddb7bb0678e18cc70e0c96243c0856fb7
1MuRaD12 <tu71tfsws@code.edu.az>
2022-03-10 08:51:51 +0300

    METHOD

---
+++ task 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+              
+
+            Console.WriteLine(natural(number:')'));
+
+        }
+        public static int natural(int number)
+        {
+            int count = 0;
+            number = 777;
+
+            while (number > 0)
+            {
+                //number = number % 10;
+                number = number / 10;
+                count++;
+            }
+            {
+                Console.WriteLine(count);
+            }
+           
+            {
+                return number;
+            }
+        
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ task 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 5, 7, 3, 9, 10, 20,50 };
+
+            Console.WriteLine(big(arr, num: arr.Length));
+        }
+
+        public static int big(int[] arr, int num)
+        {
+            int biggest = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] > biggest)
+                {
+                    biggest = arr[i];
+                }
+            {
+                return biggest;
+            }
+            
+        }
+    }
+}
